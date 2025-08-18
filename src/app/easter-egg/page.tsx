b83861e20616d99fd30e81b00e9f7b75077adbd7
DivEisrhoovereplison0 <photontimchun@gmail.com>
2025-08-18 22:56:02 +0800

    Add Easter Egg page component

---
+++ src/app/easter-egg/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EasterEggPage: React.FC = () => {
+    return (
+        <main style={{ padding: '2rem', textAlign: 'center' }}>
+            <h1>🥚 Easter Egg</h1>
+            <p>You've fuck the kpc!</p>
+        </main>
+    );
+};
+
+export default EasterEggPage;
