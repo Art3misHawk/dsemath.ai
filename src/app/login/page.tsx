1f11b64ec612807cb20b189167a8035baa43d790
DivEisrhoovereplison0 <photontimchun@gmail.com>
2025-08-23 10:49:52 +0800

    login function with custom email

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,7 @@
 'use client';
+
 import { createClient } from '@/lib/supabase/client';
+import { login, signup } from './actions'
 
 export default function LoginPage() {
   const supabase = createClient();
@@ -16,10 +18,21 @@ export default function LoginPage() {
       },
     });
   };
-
+  
   return (
     <div>
-      <div onClick={handleGoogleLogin}>Sign in with Google</div>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input id="email" name="email" type="email" required />
+        <label htmlFor="password">Password:</label>
+        <input id="password" name="password" type="password" required />
+        <button formAction={login}>Log in</button>
+        <button formAction={signup}>Sign up</button>
+      </form>
+      <div>
+        <div onClick={handleGoogleLogin}>Sign in with Google</div>
+      </div>
     </div>
-  );
+
+  )
 }
--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,9 +1,8 @@
-import { createBrowserClient } from '@supabase/ssr';
-import 'client-only';
+import { createBrowserClient } from '@supabase/ssr'
 
-export function createClient(): ReturnType<typeof createBrowserClient> {
-    return createBrowserClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
-    );
+export function createClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+  )
 }
