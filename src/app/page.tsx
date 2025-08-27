df36f4f649b0f386e0835241c5db69f347609e01
Art3misBillyChiu <art3mis1024@gmail.com>
2025-08-28 03:33:34 +0800

    fix(imports): correct casing in component imports for consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import HeroSection from "./_homepage-components/Herosection";
+import HeroSection from "./_homepage-components/herosection";
 import Features from "./_homepage-components/Features";
-import Pricing from "./_homepage-components/Pricing";
+import Pricing from "./_homepage-components/pricing";
 /*import Testimonials from "./_homepage-components/Testimonials"; */
 import FAQ from "./_homepage-components/FAQ";
 import Footer from "./_homepage-components/Footer";
