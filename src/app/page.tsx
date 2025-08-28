fc9d0ff28ea7236d50395dbc71e81ff28530832b
Art3misBillyChiu <art3mis1024@gmail.com>
2025-08-29 00:07:09 +0800

    fix: correct casing in import statements for HeroSection and Pricing components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import HeroSection from "./_homepage-components/herosection";
+import HeroSection from "./_homepage-components/Herosection";
 import Features from "./_homepage-components/Features";
-import Pricing from "./_homepage-components/pricing";
+import Pricing from "./_homepage-components/Pricing";
 /*import Testimonials from "./_homepage-components/Testimonials"; */
 import FAQ from "./_homepage-components/FAQ";
 import Footer from "./_homepage-components/Footer";
