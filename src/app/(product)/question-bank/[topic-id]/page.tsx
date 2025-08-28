1c5b2d95ac3a2c9f4311cf09dc8f92628345961e
DivEisrhoovereplison0 <photontimchun@gmail.com>
2025-08-28 23:32:50 +0800

    single question bank page continue

--- src/app/(product)/question-bank/[topic-id]/page.tsx
+++ src/app/(product)/question-bank/[topic-id]/page.tsx
@@ -1,9 +1,20 @@
-'use client';
+'use client'
 
-export default function SingleTopicQuestionBankPage({params}: ) {
+export default async function SingleTopicQuestionBankPage({ params }: {
+  params: { topicId: string };
+}) {
+  // Example: Fetch data based on topicId
+  const data = await fetchDataForTopic(params.topicId);
+
   return (
     <div>
-      gay
+      {params.topicId}
+      {/* Render data */}
     </div>
-  )
+  );
+}
+
+async function fetchDataForTopic(topicId: string) {
+  // Simulated fetch
+  return { topic: `Data for ${topicId}` };
 }
