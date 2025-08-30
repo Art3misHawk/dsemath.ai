dcd831d56967b8f8b2dbf58ae96ce82a5ad9d1d1
DivEisrhoovereplison0 <photontimchun@gmail.com>
2025-08-31 00:55:09 +0800

    copy the template and add use client

---
+++ src/app/page-decrepit.tsx
@@ -0,0 +1,23 @@
+import HeroSection from "./_components/Herosection-decrepit";
+import Features from "./_components/Features";
+// import Pricing from "./_components/Pricing-decrepit";
+/*import Testimonials from "./_homepage-components/Testimonials"; */
+// import FAQ from "./_components/FAQ-decrepit";
+import Footer from "./_components/Footer";
+
+import Hero from './_components/Hero'
+import Pricing from './_components/Pricing'
+import FAQ from './_components/FAQ'
+
+export default function LandingPage() {
+  return (
+    <div>
+      <Hero />
+      <Features />
+      <Pricing />
+      {/* <Testimonials /> */}
+      <FAQ />
+      <Footer />
+    </div>
+  );
+}
